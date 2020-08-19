f968f84415501bc109589fb31ce5bd0df750d2c7
鄧楨樺 <frogdeng0319@gmail.com>
2020-08-20 00:06:04 +0800

    init

---
+++ src/components/filpcard.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <div class="card_wrapper">
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+
+
+
+        </div>
+
+        
+    </div>
+</template>
+
+
+<script>
+import filpcard from '@/components/filpcard.vue'
+
+export default {
+    data() {
+        return {
+            card:{
+                'card_1' : '1'
+            }
+
+
+
+
+        }
+    },
+
+}
+</script>
+
---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <filpcard />
+  </div>
+</template>
+
+<script>
+import filpcard from '@/components/filpcard.vue'
+
+export default {
+  name: 'about',
+  components: {
+    filpcard,
+  },
+  data() {
+    return {
+      
+
+      
+    }
+  },
+}
+</script>
+
+
+<style lang="sass">
+      @import @/assets/sass/_page
+
+
+    
+</style>
